0c52f40a023a62a1af63425d503b2db86d9fda61
Sharathbhanu <sharathbanu43@gmail.com>
2023-07-24 00:09:39 +0100

    Authorization key added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import requests
+Authorization_Key="fe4c8fa7-c07c-422e-9573-4659750ab08b"
 app = Flask('Flask')
-
 @app.route("/")
-def hello():
-    return "He!"
+def reed_api():
+    Reed_Api="https://www.reed.co.uk/api/1.0/search"
+    Request_PARAMS={
+    'keywords':'Software',
+    'LocationName':'London'
+    }
+    result=requests.get(Reed_Api,params=Request_Params,auth=Authorization_Key)
+    result.json() 
