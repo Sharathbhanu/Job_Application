b5be9c96a7ebf2c666770ed5ce0bccd8a95f0920
Sharathbhanu <sharathbanu43@gmail.com>
2023-07-26 18:34:18 +0100

    added employer id

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ def reed_api():
     Request_Params['contract'] = 'True'
     Request_Params['minimumSalary']='60000'
     Request_Params['distanceFromLocation'] = '5'
+    Request_Params['employerId'] = '431258'
     result=requests.get(Reed_Api,params=Request_Params,auth=Authorization_Key)
     return jsonify(result.json())
