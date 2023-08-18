34da235c7bf2df89bbe1171ec34a8747497a10d3
Sharathbhanu <sharathbanu43@gmail.com>
2023-08-18 20:33:13 +0100

    check job results empty or not if no printfirstjob

--- static/scripts.js
+++ static/scripts.js
@@ -2,7 +2,11 @@ function displayJobResults(jobResults) {
     // Get the container where job results will be displayed
     const jobResultsContainer = document.querySelector(".job-results");
     jobResultsContainer.innerHTML = ""; // Clear previous results
-    
+
+    // Check if job results are available and not empty
+    if (jobResults && jobResults.results.length > 0) {
+        const job = jobResults.results[0]; // Get the details of the first job
+        
 function searchJobs() {
     // Get the user input values for search criteria
     const location = document.getElementById("location").value;
