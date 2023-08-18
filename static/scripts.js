45211f7a1cdfcf511a0fcd22a07430402a2a5f47
Sharathbhanu <sharathbanu43@gmail.com>
2023-08-18 20:31:14 +0100

    created displayjobresults function

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,18 @@
+function displayJobResults(jobResults) {
+    // Get the container where job results will be displayed
+    const jobResultsContainer = document.querySelector(".job-results");
+    jobResultsContainer.innerHTML = ""; // Clear previous results
+    
 function searchJobs() {
+    // Get the user input values for search criteria
     const location = document.getElementById("location").value;
     const keywords = document.getElementById("keywords").value;
     const distanceFromLocation = document.getElementById("distanceFromLocation").value;
     const contract = document.getElementById("contract").checked;
     const minimumSalary = document.getElementById("minimumSalary").value;
     const employerId = document.getElementById("employerId").value;
+
+    // Create an object with the search criteria
     const searchData = {
         location: location,
         keywords: keywords,
