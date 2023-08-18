9f1e02862e791db302ff0b945ea32d9a4d26c8c2
Sharathbhanu <sharathbanu43@gmail.com>
2023-08-18 17:24:45 +0100

    defined search jobs function adding request params

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-from flask import Flask, jsonify #imported all the requirements
+from flask import Flask,render_template, jsonify,request #imported all the requirements
 import requests
 from requests.auth import HTTPBasicAuth
 Authorization_Key=HTTPBasicAuth('fe4c8fa7-c07c-422e-9573-4659750ab08b','') # Provided my API key for authentication
-app = Flask('Flask')
+app = Flask(__name__)
 @app.route("/")# Defined the root route for the Flask app
-def reed_api():
+def search_jobs():
     Reed_Api="https://www.reed.co.uk/api/1.0/search" # URL of the Reed.co.uk API
     Request_Params={   # Parameters for the API request
-    'keywords':'Software',# Search keyword
-    'LocationName':'Staines Green',# Location name
-    'resultsToTake':'50',# Maximum number of results to return
-    'distanceFromLocation':'10'# Distance from the location (in miles)
-    }
+    'keywords': request.json['keywords'],# Search keyword
+    'LocationName': request.json['location'],# Location name
+    'distanceFromLocation':request.json['distanceFromLocation'],# Distance from the location (in miles)
     # Additional parameters for filtering
-    Request_Params['contract'] = 'True' # Contract type: True (contract) or False (permanent)
-    Request_Params['minimumSalary']='60000' # Minimum salary for job listings
-    Request_Params['distanceFromLocation'] = '5' # Updated distance from location to 5 miles
-    Request_Params['employerId'] = '431258'# Employer ID for specific employer
+    'contract' : request.json['contract'], # Contract type: True (contract) or False (permanent)
+    'minimumSalary': request.json['minimumSalary'], # Minimum salary for job listings
+    'employerId' : request.json['employerId']# Employer ID for specific employer
     # Send the API request to Reed.co.uk and get the response
+    }
     result=requests.get(Reed_Api,params=Request_Params,auth=Authorization_Key)
     oneresult=result.json() # API response JSON
     # Printed the first two results (for demonstration purposes)
